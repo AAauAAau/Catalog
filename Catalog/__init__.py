bddb298e303ec213d5e7bcdd944964b4677d71de
Tobi <lochmann.tobias@gmail.com>
2019-06-27 10:45:29 -0600

    add config.json and fix CreateDbAndContent.py

--- Catalog/__init__.py
+++ Catalog/__init__.py
@@ -1,17 +1,21 @@
 import os
+import json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import patch_request_class, UploadSet, configure_uploads, IMAGES
 
-
+with open(os.getcwd() +'\\Catalog\\config.json') as config_file:
+    config = json.load(config_file)
+    
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '5791628bb0b16ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = config.get('SECRET_KEY')
+#app.config['SECRET_KEY'] = '5791628bb0b16ce0c676dfde280ba245'
 #app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://postgres:postgres@localhost:5432/cata'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = config.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '\\Catalog\\_uploads'
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + config.get('UPLOADED_PHOTOS_DEST')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
--- CreateDbAndContent.py
+++ CreateDbAndContent.py
@@ -1,31 +1,28 @@
-#from Catalog import db
+import os
+import json
 from flask import Flask
 from Catalog.models import Categorie, CategorieItem, User
+from Catalog import db
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask('CreatDB')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/cata'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
-
+db.session.commit()
 db.reflect()
 db.drop_all()
 db.create_all()
 
 # Create dummy user
-user1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
+User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
              picture='https://pbs.twimg.com/profile_images/2671170543/'
              '18debd694829ed78203a5a36dd364160_400x400.png')
-db.session.add(user1)
+db.session.add(User1)
 db.session.commit()
 # Menu for UrbanBurger
-Categorie1 = Categorie(user_id=user1.id, name="Soccer")
+Categorie1 = Categorie(user_id=User1.id, name="Soccer")
 
 db.session.add(Categorie1)
 db.session.commit()
 CategorieItem2 = CategorieItem(
-    user_id=user1.id,
+    user_id=User1.id,
     name="Soccer shoes",
     description="Lorem ipsum dolor sit amet, consecte",
     price="7.50",
@@ -34,7 +31,7 @@ db.session.add(CategorieItem2)
 db.session.commit()
 
 CategorieItem2 = CategorieItem(
-    user_id=user1.id,
+    user_id=User1.id,
     name="Ball",
     description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit",
     price="2.99",
@@ -44,7 +41,7 @@ db.session.add(CategorieItem2)
 db.session.commit()
 
 CategorieItem2 = CategorieItem(
-    user_id=user1.id,
+    user_id=User1.id,
     name="Shirt",
     description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
     price="5.50",
@@ -54,13 +51,13 @@ db.session.add(CategorieItem2)
 db.session.commit()
 
 # Menu for Super Stir Fry
-Categorie2 = Categorie(user_id=user1.id, name="Basketball")
+Categorie2 = Categorie(user_id=User1.id, name="Basketball")
 
 db.session.add(Categorie2)
 db.session.commit()
 
 CategorieItem1 = CategorieItem(
-    user_id=user1.id,
+    user_id=User1.id,
     name="Ball",
     description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit",
     price="7.99",
@@ -70,7 +67,7 @@ db.session.add(CategorieItem1)
 db.session.commit()
 
 CategorieItem2 = CategorieItem(
-    user_id=user1.id,
+    user_id=User1.id,
     name="Bulls-Shirt",
     description="Lorem ipsum dolor sit amet, consectetuer adipiscing el ",
     price="25",
@@ -80,7 +77,7 @@ db.session.add(CategorieItem2)
 db.session.commit()
 
 CategorieItem3 = CategorieItem(
-    user_id=user1.id,
+    user_id=User1.id,
     name="Spicy Tuna Roll",
     description="Seared rare ahi, avocado, edamam",
     price="15",
@@ -88,25 +85,25 @@ CategorieItem3 = CategorieItem(
 
 db.session.add(CategorieItem3)
 db.session.commit()
-Categorie2 = Categorie(user_id=user1.id, name="Baseball")
+Categorie2 = Categorie(user_id=User1.id, name="Baseball")
 db.session.add(Categorie2)
 
-Categorie2 = Categorie(user_id=user1.id, name="Frisbee")
+Categorie2 = Categorie(user_id=User1.id, name="Frisbee")
 db.session.add(Categorie2)
 
-Categorie2 = Categorie(user_id=user1.id, name="Baseball")
+Categorie2 = Categorie(user_id=User1.id, name="Baseball")
 db.session.add(Categorie2)
 
-Categorie2 = Categorie(user_id=user1.id, name="Snowboarding")
+Categorie2 = Categorie(user_id=User1.id, name="Snowboarding")
 db.session.add(Categorie2)
 
-Categorie2 = Categorie(user_id=user1.id, name="Rock Climbing")
+Categorie2 = Categorie(user_id=User1.id, name="Rock Climbing")
 db.session.add(Categorie2)
 
-Categorie2 = Categorie(user_id=user1.id, name="Skating")
+Categorie2 = Categorie(user_id=User1.id, name="Skating")
 db.session.add(Categorie2)
 
-Categorie2 = Categorie(user_id=user1.id, name="Hockey")
+Categorie2 = Categorie(user_id=User1.id, name="Hockey")
 db.session.add(Categorie2)
 
 db.session.commit()
